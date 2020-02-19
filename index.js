2de58f28bd213fecb3b7d356717d330656bc26f8
KIRSTY DOUGLAS (1615998) <1615998@rgu.ac.uk>
2020-02-19 11:53:58 +0000

    index updates

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var http = require("http");
+const PORT = process.env.PORT || 3000;
 
 http.createServer(function (request, response) {
 
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.end('Hello World\n');
-}).listen(8080);
+}).listen(PORT);
 
 console.log('Server runing at http://127.0.0.1:8080/');
